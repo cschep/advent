376685d0d1209f2516c45f3034cff011d79077a1
Chris Schepman <chris@schepman.org>
2021-12-02 11:36:30 -0800

    day 2 complete

--- ruby/2021/2.rb
+++ ruby/2021/2.rb
@@ -17,6 +17,28 @@ def part1(file)
   puts result[:horizonal_position] * result[:depth]
 end
 
+Result = Struct.new(:horizonal_position, :depth, :aim)
+def part2(file)
+  result = Result.new(0, 0, 0)
+
+  file.each_line do |line|
+    command, val = line.split
+    val = val.to_i
+
+    case command
+    when 'up'
+      result.aim -= val
+    when 'down'
+      result.aim += val
+    when 'forward'
+      result.horizonal_position += val
+      result.depth += (val * result.aim)
+    end
+  end
+
+  puts result.horizonal_position * result.depth
+end
+
 File.open('2-input.txt') do |f|
-  part1(f)
+  part2(f)
 end
