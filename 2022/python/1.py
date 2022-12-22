182706ccd57d2627c9e303b27ff5408e360bf2ad
Chris Schepman <chris@schepman.org>
2022-12-22 14:13:34 -0800

    day 1 down baby

--- 2022/python/1.py
+++ 2022/python/1.py
@@ -1,1 +1,22 @@
-print("hello")
+def test():
+    print("test")
+
+with open("1.input") as file:
+    elves = {}
+    current_elf = 0
+    calories = 0
+    for line in file.readlines():
+        if line == "\n":
+            elves[current_elf] = calories
+            calories = 0
+            current_elf += 1
+        else:
+            calories += int(line)
+
+# part 1
+print(max(elves.values()))
+
+# part 2
+top3 = sorted(elves.values())[-3:]
+print(sum(top3))
+
