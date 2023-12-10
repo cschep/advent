0dd491e1a7cc1c64ccb9534e791a801c7928b2aa
Chris Schepman <chris@schepman.org>
2023-12-09 21:14:28 -0500

    day 9 part t 1

---
+++ 2023/nine/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("9.input.small")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	result := 0
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		numberStrings := strings.Split(line, " ")
+
+		lineNumbers := []int{}
+		for _, v := range numberStrings {
+			i, err := strconv.Atoi(v)
+			if err != nil {
+				panic(err)
+			}
+
+			lineNumbers = append(lineNumbers, i)
+		}
+
+		fmt.Println(lineNumbers)
+		ds := [][]int{}
+		d := diffs(lineNumbers)
+		for {
+			ds = append(ds, d)
+			if all(d, 0) {
+				break
+			}
+			d = diffs(d)
+		}
+
+		//part 1
+		nextStep := 0
+		for i := len(ds) - 1; i >= 0; i-- {
+			s := ds[i][len(ds[i])-1]
+			nextStep += s
+		}
+
+		answer := nextStep + lineNumbers[len(lineNumbers)-1]
+		result += answer
+		fmt.Println(answer)
+	}
+
+	fmt.Println(result)
+}
+
+// GENERICS!!
+func all[T comparable](a []T, same T) bool {
+	for _, v := range a {
+		if v != same {
+			return false
+		}
+	}
+
+	return true
+}
+
+func diffs(a []int) []int {
+	prev := a[0]
+	diffs := []int{}
+	for i := 1; i < len(a); i++ {
+		curr := a[i]
+		diff := curr - prev
+		diffs = append(diffs, diff)
+		prev = curr
+	}
+
+	return diffs
+}
