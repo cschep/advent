82397935172d61f90ae2f4fd82f93e5d0618cdca
Chris Schepman <chris@schepman.org>
2023-12-09 20:01:16 -0500

    day 8 part 2

--- 2023/eight/main.go
+++ 2023/eight/main.go
@@ -48,54 +48,25 @@ func main() {
 		nodeMap[key] = node
 	}
 
-	//
-	starts := []string{}
+	counts := []int{}
 	for k := range nodeMap {
 		if strings.HasSuffix(k, "A") {
-			starts = append(starts, k)
+			//find the counts for each one
+			count := traverse(k)
+			fmt.Println(k, count)
+			counts = append(counts, count)
 		}
 	}
 
-	// for {
-	// 	select {
-	// 	case <-stepChan:
-	// 		traverse(starts[0], 0, 1, stepChan)
-	// 		time.Sleep(time.Second * 5)
-	// 	default:
-	// 		println("Waiting for data")
-	// 		time.Sleep(time.Duration(math.MaxInt64))
-	// 	}
-	// }
-
-	chans := []chan string{}
-	for _, start := range starts {
-		ch := make(chan string)
-		chans = append(chans, ch)
-		go traverse(start, ch)
-	}
-
-	count := 1
-	for {
-		results := []string{}
-		allZ := true
-		for _, ch := range chans {
-			next := <-ch
-			fmt.Println(ch, next)
-			if !strings.HasSuffix(next, "Z") {
-				allZ = false
-			}
-			results = append(results, next)
-		}
-		if allZ {
-			fmt.Println("WINNER", count)
-			break
-		}
-		count++
-	}
+	// find the something common something
+	a := counts[0]
+	b := counts[1]
+	fmt.Println(LCM(a, b, counts[2:]...))
 }
 
-func traverse(key string, ch chan<- string) {
+func traverse(key string) int {
 	instructionIndex := 0
+	count := 1
 	for {
 		if instructionIndex > len(instructions)-1 {
 			instructionIndex = 0
@@ -109,9 +80,33 @@ func traverse(key string, ch chan<- string) {
 			nextKey = nodeMap[key].right
 		}
 
-		ch <- nextKey
+		if strings.HasSuffix(nextKey, "Z") {
+			return count
+		}
 
 		key = nextKey
 		instructionIndex++
+		count++
 	}
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
