31fd28c80ffb26511fffaa164f1fc1c0852aa61e
Chris Schepman <chris@schepman.org>
2023-12-10 14:13:36 -0500

    add some fancy utils

--- 2023/10/main.go
+++ 2023/10/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"aoc/util"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("TEN")
+	a := []string{"1", "2", "3", "4"}
+	b := util.Atois(a)
+
+	fmt.Println(a)
+	fmt.Println(b)
 }
--- 2023/util/util.go
+++ 2023/util/util.go
@@ -1,5 +1,7 @@
 package util
 
+import "strconv"
+
 // GENERICS!!
 func All[T comparable](a []T, same T) bool {
 	for _, v := range a {
@@ -9,4 +11,24 @@ func All[T comparable](a []T, same T) bool {
 	}
 
 	return true
+}
+
+func Map[T, U any](a []T, f func(s T) U) []U {
+	result := []U{}
+	for _, v := range a {
+		new := f(v)
+		result = append(result, new)
+	}
+
+	return result
+}
+
+func Atois(a []string) []int {
+	return Map(a, func(x string) int {
+		i, err := strconv.Atoi(x)
+		if err != nil {
+			panic(err)
+		}
+		return i
+	})
 }
