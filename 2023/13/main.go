2da504b1320ca13420c0d1117d3f7513e21b0f38
Chris Schepman <chris@schepman.org>
2024-12-29 13:02:30 -0800

    dunno

--- 2023/13/main.go
+++ 2023/13/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	f, err := os.Open("inputs/13.input.small")
+	f, err := os.Open("inputs/13.input")
 	if err != nil {
 		panic(err)
 	}
@@ -24,7 +24,6 @@ func main() {
 	g := util.Grid{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
 		if line == "" {
 			grids = append(grids, g)
 			g = util.Grid{}
@@ -36,29 +35,84 @@ func main() {
 
 	summary := 0
 	for _, g := range grids {
-		g.Print()
-		vmirror, err := checkVertical(g)
-		if err != nil {
-			fmt.Println(err)
-
-			// rotate
-			r := rotate(g)
-			// check again e.g. "horizontal"
-			hmirror, err := checkVertical(r)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Println("HORIZONTAL", hmirror)
-				summary += (hmirror + 1) * 100
-			}
-		} else {
-			// we found a vertical!
-			fmt.Println(vmirror)
-			summary += vmirror + 1
-		}
+		summary += checkAllSmudges(g)
 	}
 
 	fmt.Println("summary:", summary)
+}
+
+func checkAllSmudges(g util.Grid) int {
+	// find the original reflection line
+	orig, _ := check(g, CheckResult{})
+
+	//part2 go through each one and flip it and check everything
+	fmt.Println("CHECKING SMUDGES")
+	g.Print()
+	i := 0
+	for y := 0; y < g.Height(); y++ {
+		for x := 0; x < g.Width(); x++ {
+			i++
+			s := g.Get(x, y)
+			new := "#"
+			if s == "#" {
+				new = "."
+			}
+			g.Set(x, y, new)
+			res, found := check(g, orig)
+			g.Set(x, y, s)
+
+			if found {
+				fmt.Println("FOUND", res)
+			}
+
+			if found && !res.eq(orig) {
+				return res.amount
+			}
+
+			// if i == 1 {
+			// 	return 0
+			// }
+		}
+		fmt.Println()
+	}
+
+	panic("no solutions found for smudges")
+}
+
+type CheckResult struct {
+	location  int
+	amount    int
+	direction string
+}
+
+func (cr CheckResult) eq(cr2 CheckResult) bool {
+	return cr.location == cr2.location && cr.direction == cr2.direction && cr.amount == cr2.amount
+}
+
+//
+
+func check(g util.Grid, ignore CheckResult) (CheckResult, bool) {
+	ignoreLocation := -1
+	if ignore.direction == "v" {
+		ignoreLocation = ignore.location
+	}
+	vmirror, found := checkVertical(g, ignoreLocation)
+	if found {
+		return CheckResult{location: vmirror, amount: vmirror + 1, direction: "v"}, true
+	}
+
+	// check again rotated e.g. "horizontal"
+	r := rotate(g)
+	ignoreLocation = -1
+	if ignore.direction == "h" {
+		ignoreLocation = ignore.location
+	}
+	hmirror, found := checkVertical(r, ignoreLocation)
+	if found {
+		return CheckResult{location: hmirror, amount: (hmirror + 1) * 100, direction: "h"}, true
+	}
+
+	return CheckResult{}, false
 }
 
 // 90 deg counter clockwise
@@ -76,20 +130,21 @@ func rotate(g util.Grid) util.Grid {
 }
 
 func checkColumns(x1, x2 int, g util.Grid) bool {
-	// fmt.Println("checking", x1, x2)
 	found := true
 	for y := 0; y < g.Height(); y++ {
 		if g.Get(x1, y) != g.Get(x2, y) {
 			found = false
 		}
 	}
-	// fmt.Println(found)
 	return found
 }
 
-func checkVertical(g util.Grid) (int, error) {
+func checkVertical(g util.Grid, ignore int) (int, bool) {
 	startx := -1
 	for x := 0; x < g.Width(); x++ {
+		if x == ignore {
+			continue
+		}
 		if g.Get(x, 0) == g.Get(x+1, 0) {
 			startx = x
 			found := checkColumns(x, x+1, g)
@@ -102,12 +157,12 @@ func checkVertical(g util.Grid) (int, error) {
 					x2 = x2 + 1
 				}
 				if x1 < 0 || x2 > g.Width()-1 {
-					fmt.Println("WINNER")
-					return startx, nil
+					// fmt.Println("WINNER", startx)
+					return startx, true
 				}
 			}
 		}
 	}
 
-	return -1, fmt.Errorf("not vertical mirror found")
+	return -1, false
 }
