2681225124dea44f4b3ac614b4cf66e22a3a4676
Chris Schepman <chris@schepman.org>
2025-12-05 18:08:05 -0800

    day 1!

--- 2025/aocplusplus/day01/main.cpp
+++ 2025/aocplusplus/day01/main.cpp
@@ -8,42 +8,64 @@ int main() {
     if (ifstream file("../day01/input-1.txt"); file.is_open()) {
         int password = 0;
         int current = 50;
+        int prev = current;
+
+        // part 2
+        const bool password_method_0x434C49434B = true;
 
         string line;
         while (getline(file, line)) {
             const char direction = line[0];
             const int num = stoi(line.substr(1, line.length()));
 
-            if (direction == 'L') {
-                current -= num;
-                if (current < 0) {
-                    const int unit = current % 10;
-                    const int tens = (current / 10) % 10;
-                    const int rest = tens*10 + unit;
-                    current = 100 + rest;
-                }
-                // TODO: understand better why this doesn't happen on 'R' ?
-                if (current == 100) { current = 0; }
-            } else if (direction == 'R') {
-                current += num;
-                if (current > 99) {
-                    const int unit = current % 10;
-                    const int tens = (current / 10) % 10;
-                    const int rest = tens*10 + unit;
-                    current = rest;
-                }
+            const int positive_hundreds = abs(num / 100);
+            const int new_num = num - (positive_hundreds * 100);
+
+            // remove and deal with the even hundreds
+            if (password_method_0x434C49434B && positive_hundreds > 0) {
+                cout << "adding " << positive_hundreds << " to passwords" << endl;
+                password += positive_hundreds;
             }
 
+            cout << "starting at " << current << " moving " << direction << " " << num << " / " << new_num << endl;
+
+            // which direction are we going?
+            prev = current;
+            if (direction == 'L') {
+                current -= new_num;
+            } else if (direction == 'R') {
+                current += new_num;
+            }
+
+            // going around right
+            if (current > 99) {
+                if (password_method_0x434C49434B && current != 100) {
+                    cout << "passed 0 right" << endl;
+                    password++;
+                }
+                current -= 100;
+            }
+
+            // going around left
+            if (current < 0) {
+                if (password_method_0x434C49434B && prev != 0) {
+                    cout << "passed 0 left" << endl;
+                    password++;
+                }
+                current = 100 + current;
+            }
+
+            // if we hit zero on the nose, add it
             if (current == 0) {
+                cout << "landed on 0!, password++" << endl;
                 password++;
             }
-            cout << direction << " " << num << " " << current << endl;
         }
 
         cout << "password: " << password << endl;
     } else {
         cout << "Unable to open file!" << endl;
     }
+
     return 0;
 }
-
