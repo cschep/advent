bad4048986e2899c9c5624669659ed4e0cbfcfc5
Chris Schepman <chris@schepman.org>
2025-12-05 19:28:14 -0800

    2025 day 2 part 1

--- 2025/aocplusplus/day02/main.cpp
+++ 2025/aocplusplus/day02/main.cpp
@@ -1,8 +1,42 @@
+#include <fstream>
 #include <iostream>
+#include <ostream>
+#include <sstream>
+#include <cstdint>
 
 using namespace std;
 
 int main() {
-    cout << "DAY 2" << endl;
-    return 0;
+    if (ifstream file("../day02/input-2.txt"); file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            vector<int64_t> invalid_ids;
+            stringstream ss(line);
+            string range;
+            while (getline(ss, range, ',')) {
+                size_t split_at = range.find('-');
+                int64_t bottom = stoll(range.substr(0, split_at));
+                int64_t top = stoll(range.substr(split_at + 1, range.length()));
+                // cout << range << endl;
+
+                for (int64_t id = bottom; id <= top; id++) {
+                    string str = to_string(id);
+                    string first_half = str.substr(0, str.length()/2);
+                    string second_half = str.substr(str.length()/2, str.length());
+                    if (first_half == second_half) {
+                        // cout << first_half << " == " << second_half << endl;
+                        invalid_ids.push_back(id);
+                    }
+                }
+            }
+
+            int64_t total = 0;
+            for (const int64_t id: invalid_ids) {
+                total += id;
+            }
+            cout << "total:" << total << endl;
+        }
+    } else {
+        cout << "Unable to open file!" << endl;
+    }
 }
