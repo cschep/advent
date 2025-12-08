d24030e6c41336e6f85396403f95b9dd2c9ffe86
Chris Schepman <chris@schepman.org>
2025-12-08 15:12:48 -0800

    day 1 clean up

--- 2025/1/main.cpp
+++ 2025/1/main.cpp
@@ -3,7 +3,7 @@
 #include <ostream>
 
 int main() {
-    if (std::ifstream file("../day01/input-1.txt"); file.is_open()) {
+    if (std::ifstream file("1/1.input"); file.is_open()) {
         int password = 0;
         int current = 50;
         int prev = current;
@@ -11,7 +11,7 @@ int main() {
         // part 2
         const bool password_method_0x434C49434B = true;
 
-		std::string line;
+        std::string line;
         while (getline(file, line)) {
             const char direction = line[0];
             const int num = stoi(line.substr(1, line.length()));
@@ -21,11 +21,12 @@ int main() {
 
             // remove and deal with the even hundreds
             if (password_method_0x434C49434B && positive_hundreds > 0) {
-				std::cout << "adding " << positive_hundreds << " to passwords" << std::endl;
+                std::cout << "adding " << positive_hundreds << " to passwords" << std::endl;
                 password += positive_hundreds;
             }
 
-			std::cout << "starting at " << current << " moving " << direction << " " << num << " / " << new_num << std::endl;
+            std::cout << "starting at " << current << " moving " << direction << " " << num << " / " << new_num
+                      << std::endl;
 
             // which direction are we going?
             prev = current;
@@ -38,7 +39,7 @@ int main() {
             // going around right
             if (current > 99) {
                 if (password_method_0x434C49434B && current != 100) {
-					std::cout << "passed 0 right" << std::endl;
+                    std::cout << "passed 0 right" << std::endl;
                     password++;
                 }
                 current -= 100;
@@ -47,7 +48,7 @@ int main() {
             // going around left
             if (current < 0) {
                 if (password_method_0x434C49434B && prev != 0) {
-					std::cout << "passed 0 left" << std::endl;
+                    std::cout << "passed 0 left" << std::endl;
                     password++;
                 }
                 current = 100 + current;
@@ -55,14 +56,14 @@ int main() {
 
             // if we hit zero on the nose, add it
             if (current == 0) {
-				std::cout << "landed on 0!, password++" << std::endl;
+                std::cout << "landed on 0!, password++" << std::endl;
                 password++;
             }
         }
 
-		std::cout << "password: " << password << std::endl;
+        std::cout << "password: " << password << std::endl;
     } else {
-		std::cout << "Unable to open file!" << std::endl;
+        std::cout << "Unable to open file!" << std::endl;
     }
 
     return 0;
