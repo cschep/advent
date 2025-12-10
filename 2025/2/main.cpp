87850b8e427dcee24a8608afc1477c4c2a48950d
Chris Schepman <chris@schepman.org>
2025-12-10 11:44:39 -0800

    refactor day 2 part 1 to make part 2 easier

--- 2025/2/main.cpp
+++ 2025/2/main.cpp
@@ -2,9 +2,39 @@
 #include <fstream>
 #include <iostream>
 #include <ostream>
+#include <print>
 #include <sstream>
+#include <vector>
 
 using namespace std;
+
+/*
+ * for 1 to the half the length of the string
+ *   make a substring of that length, and a substring of the rest
+ *   loop through rest in chunks of size
+ *   	if they !eq the main chunk, bail on this id
+ *   	if you get all the way through the loop store the id as invalid
+ */
+
+bool check_invalid(string id, int size) {
+    // part 1
+    // all odd id's are valid
+    if (id.length() % 2 > 0) {
+        return false;
+    }
+
+    string new_id = id.substr(0, size);
+    string rest = id.substr(size, id.length());
+
+    for (int i = 0; i < rest.length(); i += size) {
+        string chunk = rest.substr(i, size);
+        if (new_id != chunk) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
     if (ifstream file("2/2.input.small"); file.is_open()) {
@@ -17,13 +47,14 @@ int main() {
                 size_t split_at = range.find('-');
                 int64_t bottom = stoll(range.substr(0, split_at));
                 int64_t top = stoll(range.substr(split_at + 1, range.length()));
-                // cout << range << endl;
+                cout << range << endl;
 
                 for (int64_t id = bottom; id <= top; id++) {
-                    string str = to_string(id);
-                    string first_half = str.substr(0, str.length() / 2);
-                    string second_half = str.substr(str.length() / 2, str.length());
-                    if (first_half == second_half) {
+                    string id_str = to_string(id);
+                    int size = id_str.length() / 2;
+                    println("checking {} with size {}", id_str, size);
+                    if (check_invalid(id_str, size)) {
+                        println("{} invalid!", id_str);
                         invalid_ids.push_back(id);
                     }
                 }
