3853c70338401e096bc6c7104e60d359fbb1b9eb
Chris Schepman <chris@schepman.org>
2025-12-15 20:54:39 -0800

    day 3 part 2 !

--- 2025/3/main.cpp
+++ 2025/3/main.cpp
@@ -4,48 +4,71 @@
 #include <print>
 #include <string>
 
+std::string max_twelve(std::string bank) {
+    std::string result = "000000000000";
+    if (bank.length() < 2) {
+        return result;
+    }
+
+    // for each slot in the result
+    int start_search_from = 0;
+    for (int i = 0; i < result.length(); i++) {
+        // std::println("choosing {} {}", i, result[i]);
+
+        // find the biggest number while leaving enough slots left to finish
+        int still_need = result.length() - 1 - i;
+        int biggest = -1;
+        for (int idx = start_search_from; idx < bank.length() - still_need; idx++) {
+            int current = bank[idx] - '0';
+            if (current > biggest) {
+                biggest = current;
+                start_search_from = idx + 1;
+            }
+        }
+
+        result[i] = biggest + '0';
+    }
+
+    return result;
+}
+
 int max_joltage(std::string bank) {
     if (bank.length() < 2) {
         return 0;
     }
+
     // find highest with at least one digit after it
-    //
     int highest = -1;
     int highest_index = -1;
     for (int i = 0; i < bank.length() - 1; i++) {
         int current = bank[i] - '0';
         if (current > highest) {
-            std::println("new highest: {}", current);
             highest = current;
             highest_index = i;
         }
     }
 
     // now that we have the highest find the second highest after it?
-    //
     int next_highest = -1;
     for (int i = highest_index + 1; i < bank.length(); i++) {
         int current = bank[i] - '0';
         if (current > next_highest) {
-            std::println("new next highest: {}", current);
             next_highest = current;
         }
     }
 
-    int joltage = std::stoi(std::format("{}{}", highest, next_highest));
-
-    std::println("{}", joltage);
-
-    return joltage;
+    return std::stoi(std::format("{}{}", highest, next_highest));
 }
 
 int main() {
-    int result = 0;
+    int64_t result = 0;
     if (std::ifstream file("3/3.input"); file.is_open()) {
         std::string line;
         while (getline(file, line)) {
-            std::cout << line << std::endl;
-            result += max_joltage(line);
+            // std::cout << line << std::endl;
+            std::string rez = max_twelve(line);
+            // std::cout << rez << std::endl;
+            result += std::stol(rez);
         }
     } else {
         std::cout << "Unable to open file!" << std::endl;
